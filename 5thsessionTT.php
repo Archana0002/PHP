54c33841828443b9d24593cb87de60c182617a69
Archana prakasan <85437105+Archana0002@users.noreply.github.com>
2023-12-13 10:50:20 +0530

    Add files via upload

---
+++ 5thsessionTT.php
@@ -0,0 +1,52 @@
+<html>
+   <head>
+	<title>EXAM</title>
+	<style>
+	table{
+	   
+	   background-color:aquamarine;
+	}
+	body
+	{
+	  font-family:Arial;
+	  background-color:aqua;
+	}
+	</style>
+   </head>
+     <body>
+	<center>
+	   <h1>
+	     <u><b>SESSI0N EXAMINATION TIMETABLE</b></u>
+	   </h1>
+	     <table border="2">
+	      <tr>
+	       <th>DATE</th>
+	       <th>SUBJECT</th>
+	       <th>Time</th>
+	      </tr>
+	      <tr>
+	        <td>22/11/2023</td>
+	        <td>Maths</td>
+	        <td>1.00PM TO 4.00PM</td>
+	      </tr>
+	      <tr>
+	        <td>23/11/2023</td>
+	        <td>ADS</td>
+	        <td>1.00PM TO 4.00PM</td>
+	      </tr>
+	      <tr>
+	        <td>24/11/2023</td>
+	        <td>Digital</td>
+	        <td>1.00PM TO 4.00PM</td>
+	      </tr>
+	      <tr>
+	        <td>25/11/2023</td>
+	        <td>ASE</td>
+	        <td>1.00PM TO 4.00PM</td>
+	      </tr>
+	     </table>
+	 </center>
+      </body>
+</html>
+	      
+
