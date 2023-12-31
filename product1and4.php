38662b563a55977a4948ce1a4cb0cd2ef89777f7
Archana prakasan <85437105+Archana0002@users.noreply.github.com>
2023-12-31 15:21:55 +0530

    Add files via upload

---
+++ product1and4.php
@@ -0,0 +1,71 @@
+<?php
+
+$con=new mysqli("localhost","root","","stock");
+?>
+
+<html>
+  <head>
+    <title>Product</title>
+  </head>
+     <body>
+       <center>
+	<h1>PRODUCTS</h1>
+	<form method="POST">
+	<table cellpadding="10px" border="1">
+        <?php
+           for($i=0;$i<4;$i++)
+           {
+		$name="name".$i;
+		$quantity="quantity".$i;
+		$price="price".$i;
+		$mdate="mdate".$i;
+		$edate="edate".$i;
+	   ?>
+	 <tr>
+	  <td>item name</td>
+	  <td><input type="text" name=<?php echo $name ?> ></td>
+	 
+	  <td>quantity</td>
+	  <td><input type="number" name=<?php echo $quantity ?> ></td>
+	 
+	  <td>price</td>
+	  <td><input type="number" name=<?php echo $price ?> ></td>
+	 
+	  <td>manufacturer date</td>
+	  <td><input type="date" name=<?php echo $mdate ?> ></td>
+	 
+	  <td>expiry date</td>
+	  <td><input type="date" name=<?php echo $edate ?> ></td>
+	 </tr>
+	<?php   }  ?>
+	</table><br>
+	<input type="submit" value="submit" name="btn1">
+	</form>
+       </center>
+     </body>
+</html>
+
+
+<?php
+
+if(isset($_POST['btn1']))
+{
+   for($i=0;$i<4;$i++)
+   {
+    $name="name".$i;
+    $quantity="quantity".$i;
+    $price="price".$i;
+    $mdate="mdate".$i;
+    $edate="edate".$i;
+
+    $ins1="INSERT INTO product(name,quantity,price,mdate,edate)VALUES('$_POST[$name]','$_POST[$quantity]','$_POST[$price]','$_POST[$mdate]','$_POST[$edate]')";
+    $con->query($ins1);
+
+   }
+
+}
+
+?>
+    
+
+	
---
+++ product5(retrieve).php
@@ -0,0 +1,40 @@
+<?php
+
+$con=new mysqli("localhost","root","","stock");
+$c="select * from product";
+$d=$con->query($c);
+?>
+
+
+<html>
+<head>
+<title></title>
+</head>
+  <body>
+  <center>	
+	<table border="1" cellpadding="10px">
+	 <tr>
+	   <th>name</th>
+	   <th>quantity</th>
+ 	   <th>price</th>
+ 	   <th>manufacturedate</th>
+ 	   <th>expirydate</th>
+	 </tr>
+	 <?php
+	 while($p=$d->fetch_assoc())
+	 {
+	 ?>
+	<tr>
+	
+	<td><?php echo $p['name'] ?> </td>
+	<td><?php echo $p['quantity'] ?> </td>
+	<td><?php echo $p['price'] ?> </td>
+	<td><?php echo $p['mdate'] ?> </td>
+	<td><?php echo $p['edate'] ?> </td>
+	</tr>
+
+<?php  }  ?>
+	</table>
+   </center>
+   </body>
+</html>
